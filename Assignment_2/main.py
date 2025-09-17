a97a23f1df63854e7d209f2fadd599f518ad9498
Dark <>
2025-09-17 13:30:38 +0530

    ass 2 op added

--- Assignment_2/main.py
+++ Assignment_2/main.py
@@ -1,6 +1,12 @@
 def add_numbers(numbers):
     return sum(numbers)
 
+def my_add(*n):
+    res =0
+    for num in n:
+        res += num
+    return res
+
 
 def performAction(mylist, n):
     return [mylist[i:i+n] for i in range(0, len(mylist), n)]
@@ -45,20 +51,27 @@ def separate_numbers(iterable):
 
 
 nums = [10, 20, 30, 40]
-print("Sum:", add_numbers(nums))
+#print("Sum:", add_numbers(nums))
 
+print("Sum :" , my_add(2,25,4,3242,324,24,24,234,2,34,24,2,4,234,234,2,42))
+
+print("-" * 40)
 
 data = [1, 2, 3, 4, 5, 6, 7]
 print("Split list:", performAction(data, 3))
 
-
+print("-" * 40)
 
 
 names = ["John", "Seth", "Jane", "Ben", "Priyanka"]
 surnames = ["Doe", "Rollins", "Smith", "Dover", "Chopra"]
+
+print(names)
+print(surnames)
+print("-" * 40)
 print("Full names:", combine_names(names, surnames))
 
-
+print("-" * 40)
 
 data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 odd_list, even_list, prime_list = separate_numbers(data)
